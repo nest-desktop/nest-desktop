b70ec9328169950c0edfed8c15c14434f24223bb
Sebastian Spreizer <spreizer@web.de>
2021-01-15 15:36:45 +0100

    Update vue config

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
-const { NewLineKind } = require("typescript");
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const { NewLineKind } = require('typescript');
 
 module.exports = {
-  transpileDependencies: ["vuetify"],
-  outputDir: "./nest_desktop/app",
+  transpileDependencies: ['vuetify'],
+  outputDir: './nest_desktop/app',
   // https://stackoverflow.com/questions/55258355/vue-clis-type-checking-service-ignores-memory-limits#55810460
   // and https://cli.vuejs.org/config/#parallel
   configureWebpack: config => {
@@ -20,9 +20,9 @@ module.exports = {
     // copy the options from the original ones, but modify memory and CPUs
     const newForkTsCheckerOptions = existingForkTsChecker.options;
     newForkTsCheckerOptions.memoryLimit = 8192;
-    newForkTsCheckerOptions.workers = require("os").cpus().length - 1;
+    newForkTsCheckerOptions.workers = require('os').cpus().length - 1;
     config.plugins.push(
       new ForkTsCheckerWebpackPlugin(newForkTsCheckerOptions)
     );
-  }
+  },
 };
