314d77248d68137615d6b2aee236f65d427b8006
Sebastian Spreizer <spreizer@web.de>
2021-01-18 10:32:24 +0100

    Add dockerfile for dev1

--- docker/docker-compose/nest-desktop/entrypoint.sh
+++ docker/docker-compose/nest-desktop/entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# NEST environment
-source /opt/nest/bin/nest_vars.sh
-
-# Start NEST Server
-nest-server start -d -h 0.0.0.0 -u nest
-
 # Start NEST Desktop
 nest-desktop start -h 0.0.0.0
