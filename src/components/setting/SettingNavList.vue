509b10f04a99509a9438660512192cbd43667e24
Sebastian Spreizer <spreizer@web.de>
2021-01-20 12:19:43 +0100

    Settings

--- src/components/setting/SettingNavList.vue
+++ src/components/setting/SettingNavList.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="settings">
-    <v-list dense>
-      <v-list-item
-        :key="setting"
-        v-for="setting in settings"
-        v-text="setting"
-        :to="'/setting/' + setting"
-      >
-        <v-list-item-content>
-          <v-list-item-title v-text="setting" />
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
+  <div class="settingNavList">
+    <v-row no-gutters>
+      <v-col>
+        <v-list dense>
+          <v-list-item
+            :key="setting"
+            v-for="setting in settings"
+            v-text="setting"
+            :to="'/setting/' + setting"
+          >
+            <v-list-item-content>
+              <v-list-item-title v-text="setting" />
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -19,9 +23,11 @@
 import Vue from 'vue';
 
 export default Vue.extend({
-  name: 'Settings',
-  data: () => ({
-    settings: ['App', 'NEST Server', 'Model', 'Project'],
-  }),
+  name: 'SettingNavList',
+  setup() {
+    return {
+      settings: ['app', 'backend', 'model', 'project'],
+    };
+  },
 });
 </script>
