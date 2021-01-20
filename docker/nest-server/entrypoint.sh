ff63768384ea22682bea95d90b6d69ce2a0ef622
Sebastian Spreizer <spreizer@web.de>
2021-01-20 13:33:32 +0100

    Increase buffer size

--- docker/nest-server/entrypoint.sh
+++ docker/nest-server/entrypoint.sh
@@ -5,4 +5,4 @@
 export NEST_SERVER_MODULES=nest,numpy
 export NEST_SERVER_RESTRICTION_OFF=true
 
-uwsgi --module nest.server:app --http-socket 0.0.0.0:5000 --buffer-size 32768
+uwsgi --module nest.server:app --http-socket 0.0.0.0:5000 --buffer-size 65535
