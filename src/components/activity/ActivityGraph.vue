52db3195dc41caebb124cf74dea4b2ddf2349024
Sebastian Spreizer <spreizer@web.de>
2021-01-21 17:49:52 +0100

    Update activity graph

--- src/components/activity/ActivityGraph.vue
+++ src/components/activity/ActivityGraph.vue
@@ -1,38 +1,74 @@
 <template>
-  <div class="activityGraph" v-if="state.graph.data">
+  <div class="activityGraph" v-if="state.graph">
     <Plotly
-      :config="state.graph.config"
-      :data="state.graph.data"
-      :layout="state.graph.layout"
-      :style="state.graph.style"
+      :config="state.graph.activityChartGraph.config"
+      :data="state.graph.activityChartGraph.data"
+      :layout="state.graph.activityChartGraph.layout"
+      :style="state.graph.activityChartGraph.style"
       :useResizeHandler="true"
+      v-if="state.view == 'abstract'"
+    />
+
+    <ActivityAnimationGraph
+      :graph="state.graph.activityAnimationGraph"
+      v-if="state.view == 'spatial'"
     />
   </div>
 </template>
 
 <script>
 import Vue from 'vue';
-import { reactive, watch } from '@vue/composition-api';
+import { reactive, onMounted, watch } from '@vue/composition-api';
 import { Plotly } from 'vue-plotly';
+
+import ActivityAnimationGraph from '@/components/activity/ActivityAnimationGraph.vue';
 
 export default Vue.extend({
   name: 'ActivityGraph',
   components: {
     Plotly,
+    ActivityAnimationGraph,
   },
   props: {
     graph: Object,
+    projectId: String,
+    view: String,
   },
   setup(props) {
     const state = reactive({
-      graph: props.graph.activityChartGraph,
+      graph: props.graph,
+      view: props.view,
     });
+
+    const update = () => {
+      state.graph = null;
+      setTimeout(() => {
+        state.graph = props.graph;
+        state.view = state.graph.project.hasSpatialActivities
+          ? props.view
+          : 'abstract';
+      }, 1);
+    };
+
     watch(
       () => props.graph,
-      graph => {
-        state.graph = graph.activityChartGraph;
-      }
+      () => update()
     );
+
+    watch(
+      () => props.view,
+      () => update()
+    );
+
+    watch(
+      () => props.projectId,
+      () => update()
+    );
+
+    onMounted(() => {
+      update();
+    });
+
     return { state };
   },
 });
