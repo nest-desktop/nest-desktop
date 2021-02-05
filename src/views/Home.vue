6bc847d1081ca3e5838fc5791473d660c0291201
Sebastian Spreizer <spreizer@web.de>
2021-02-05 12:54:33 +0100

    Update Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,18 @@
 <template>
   <div>
-    <!-- <v-app-bar app clipped-left color="primary" dark dense flat>
-      NEST Desktop
+    <!-- <v-app-bar app clipped-left color="black" dark dense flat>
+      <v-toolbar-title>
+        NEST Desktop
+      </v-toolbar-title>
     </v-app-bar> -->
-
-    <hello-world />
   </div>
 </template>
 
 <script>
 import Vue from 'vue';
-import HelloWorld from '../components/HelloWorld.vue';
 
 export default Vue.extend({
   name: 'Home',
-
-  components: {
-    HelloWorld,
-  },
+  components: {},
 });
 </script>
