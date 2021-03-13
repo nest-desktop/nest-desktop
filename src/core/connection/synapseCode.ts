2d1fab19059b73aeb56a35ed68542ac42d42017f
Sebastian Spreizer <spreizer@web.de>
2021-03-13 20:05:41 +0100

    Update synapse code

--- src/core/connection/synapseCode.ts
+++ src/core/connection/synapseCode.ts
@@ -14,45 +14,17 @@ export class SynapseCode extends Code {
   synSpec(): string {
     const synSpecList: string[] = [];
     if (this._synapse.modelId !== 'static_synapse') {
-      synSpecList.push(this._() + `"model": "${this._synapse.modelId}"`);
+      synSpecList.push(`"model": "${this._synapse.modelId}"`);
     }
-    this._synapse.params
-      .filter((param: ModelParameter) => param.visible)
-      .forEach((param: ModelParameter) => {
-        if (!param.isRandom()) {
-          synSpecList.push(
-            this._() + `"${param.id}": ${this.format(param.value)}`
-          );
-        } else {
-          const specs: string = param.specs
-            .map(spec => this.format(spec.value))
-            .join(', ');
-          if (!param.type.startsWith('spatial')) {
-            synSpecList.push(
-              this._() + `"${param.id}": nest.${param.type}(${specs})`
-            );
-          } else if (param.type === 'spatial.distance') {
-            let value = `nest.${param.type}`;
-            if (param.specs[0].value !== 1) {
-              value += ` * ${param.specs[0].value}`;
-            }
-            if (param.specs[1].value !== 0) {
-              value += ` + ${param.specs[1].value}`;
-            }
-            synSpecList.push(this._() + `"${param.id}": ${value}`);
-          } else {
-            synSpecList.push(
-              this._() +
-                `"${param.id}": nest.${param.type}(nest.spatial.distance, ${specs})`
-            );
-          }
-        }
-      });
+
+    this._synapse.filteredParams.forEach((param: ModelParameter) =>
+      synSpecList.push(param.toCode())
+    );
 
     let script = '';
     if (synSpecList.length > 0) {
-      script += ', syn_spec={';
-      script += synSpecList.join(',');
+      script += ', syn_spec={' + this._();
+      script += synSpecList.join(',' + this._());
       script += this.end() + '}';
     }
     return script;
