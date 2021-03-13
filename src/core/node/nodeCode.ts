ee3aa818124549697c449c685031a7c57f58d0f7
Sebastian Spreizer <spreizer@web.de>
2021-03-13 20:04:42 +0100

    Update node code

--- src/core/node/nodeCode.ts
+++ src/core/node/nodeCode.ts
@@ -22,12 +22,20 @@ export class NodeCode extends Code {
   create(): string {
     let script = '';
     script += `${this.label} = nest.Create("${this._node.modelId}"`;
-    if (!this._node.spatial.hasPositions()) {
+    const addSizeInSpatial: boolean =
+      !this._node.spatial.hasPositions() ||
+      (this._node.spatial.hasPositions() &&
+        this._node.spatial.positions.name === 'free');
+    if (addSizeInSpatial && this._node.model.elementType !== 'recorder') {
       script += `, ${this._node.size}`;
     }
-    script += this.nodeParams();
+    const params: string = this.nodeParams();
+    if (params.length > 0) {
+      script += ', params=' + this.nodeParams();
+    }
     if (this._node.spatial.hasPositions()) {
-      script += this.nodeSpatial();
+      script +=
+        ',' + this._() + 'positions=' + this._node.spatial.positions.toCode();
     }
     script += ')';
     return script + '\n';
@@ -42,53 +50,21 @@ export class NodeCode extends Code {
     if (this._node.params === undefined || this._node.params.length === 0) {
       return script;
     }
-    const paramsList: string[] = [];
-    this._node.params
-      .filter((param: ModelParameter) => param.visible)
-      .forEach((param: ModelParameter) => {
-        if (param.type === 'constant') {
-          paramsList.push(
-            this._() + `"${param.id}": ${this.format(param.value)}`
-          );
-        } else {
-          const specs: string = param.specs
-            .map(spec => this.format(spec.value))
-            .join(', ');
-          paramsList.push(
-            this._() + `"${param.id}": nest.${param.type}(${specs})`
-          );
-        }
-      });
+
+    const params: string[] = this._node.filteredParams.map(
+      (param: ModelParameter) => param.toCode()
+    );
+
     if (this._node.model.existing === 'multimeter') {
       const recordFrom: string[] = this._node.recordFrom.map(
         (record: string) => '"' + record + '"'
       );
-      paramsList.push(this._() + `"record_from": [${recordFrom.join(',')}]`);
+      params.push(`"record_from": [${recordFrom.join(',')}]`);
     }
-    if (paramsList.length > 0) {
-      script += ', params={';
-      script += paramsList.join(',');
+    if (params.length > 0) {
+      script += '{' + this._();
+      script += params.join(',' + this._());
       script += this.end() + '}';
-    }
-    return script;
-  }
-
-  nodeSpatial(): string {
-    let script = '';
-    script += ', positions=';
-    const positions = this._node.spatial.toJSON();
-    if (this._node.spatial.positions.name === 'free') {
-      if (positions.pos.length > 0) {
-        // fixed free positions
-        script += `nest.spatial.free([${positions.pos
-          .map((p: number[]) => `[${p[0].toFixed(2)},${p[1].toFixed(2)}]`)
-          .join(',')}])`;
-      } else {
-        // generative free positions
-        script += `nest.spatial.free(nest.random.uniform(-0.5, 0.5), num_dimensions=2)`;
-      }
-    } else {
-      script += `nest.spatial.grid(${JSON.stringify(positions.shape)})`;
     }
     return script;
   }
