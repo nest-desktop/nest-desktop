12b37617eaed07f0ab7eecbe2434ccad59488b67
Sebastian Spreizer <spreizer@web.de>
2021-04-19 11:30:10 +0200

    Reorganize backend structure

---
+++ src/core/backends/nest.ts
@@ -0,0 +1,68 @@
+import { Backend } from './backend';
+
+export class NESTServer extends Backend {
+  private _state: any = {
+    serverReady: false,
+    simulatorReady: false,
+    nestVersion: '',
+  };
+
+  constructor() {
+    super('NESTServer');
+    this.check();
+  }
+
+  get state(): any {
+    return this._state;
+  }
+
+  check(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      // console.log('Check backend')
+      if (this.config.hostname) {
+        this.ping(this.url, () => resolve(this));
+      } else {
+        this.seek().then(() => resolve(this));
+      }
+    });
+  }
+
+  seek(): Promise<any> {
+    const protocol: string = window.location.protocol;
+    const hostname: string = window.location.hostname || 'localhost';
+    const hosts: string[] = [
+      hostname + '/nest',
+      hostname + ':' + (this.port || '5000'),
+    ];
+    const hostPromises: any[] = hosts.map(
+      (host: string) =>
+        new Promise((resolve, reject) => {
+          const url: string = protocol + '//' + host;
+          this.ping(url, () => resolve());
+        })
+    );
+    return Promise.all(hostPromises);
+  }
+
+  ping(url: string, callback: any = false): void {
+    this._state.serverReady = false;
+    return this.httpClient.ping(url, (req: any) => {
+      let resp: any;
+      switch (req.status) {
+        case 200:
+          this.url = url;
+          resp = JSON.parse(req.responseText);
+          this._state.serverReady = true;
+          this._state.simulatorReady = 'nest' in resp;
+          this._state.simulatorVersion = resp.nest;
+          if (callback) {
+            callback();
+          }
+          break;
+        case 502:
+          console.log(req);
+          break;
+      }
+    });
+  }
+}
