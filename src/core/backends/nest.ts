2cf1e822d9ab7b76bd7ad931ad921d2370654ba7
Sebastian Spreizer <spreizer@web.de>
2021-04-21 10:32:15 +0200

    add iframe to fix backend issue

--- src/core/backends/nest.ts
+++ src/core/backends/nest.ts
@@ -9,7 +9,7 @@ export class NESTServer extends Backend {
 
   constructor() {
     super('NESTServer');
-    this.check();
+    this.check().then(() => this.setUrlIFrame());
   }
 
   get state(): any {
@@ -55,6 +55,7 @@ export class NESTServer extends Backend {
    */
   ping(url: string, callback: any = false): void {
     this._state.serverReady = false;
+    this.reloadIFrame();
     return this.httpClient.ping(url, (req: any) => {
       let resp: any;
       switch (req.status) {
