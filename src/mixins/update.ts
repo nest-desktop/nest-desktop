c6ad60f7f621d91cdc5b705623bc86f071a03ed4
Sebastian Spreizer <spreizer@web.de>
2021-07-12 11:37:14 +0200

    Add mixin for updateApp

---
+++ src/mixins/update.ts
@@ -0,0 +1,60 @@
+// src/mixins/update.js
+// https://dev.to/drbragg/handling-service-worker-updates-in-your-vue-pwa-1pip
+import core from '@/core';
+
+
+export default {
+  data() {
+    return {
+      // refresh variables
+      refreshing: false,
+      registration: null,
+      updateExists: false,
+    };
+  },
+
+  created() {
+    // Listen for our custom event from the SW registration
+    document.addEventListener('swUpdated', this.updateAvailable, {
+      once: true,
+    });
+
+    // Prevent multiple refreshes
+    // navigator.serviceWorker.addEventListener('controllerchange', () => {
+    //   if (this.refreshing) return;
+    //   this.refreshing = true;
+    //   // Here the actual reload of the page occurs
+    //   window.location.reload();
+    // });
+  },
+
+  methods: {
+    // Store the SW registration so we can send it a message
+    // We use `updateExists` to control whatever alert, toast, dialog, etc we want to use
+    // To alert the user there is an update they need to refresh for
+    updateAvailable(event: any) {
+      this.registration = event.detail;
+      if (core.app.config.autoUpdate) {
+        this.refreshApp()
+      } else {
+        this.updateExists = true;
+      }
+    },
+
+    // Called when the user accepts the update
+    refreshApp() {
+      this.updateExists = false;
+
+      if (this.refreshing) return;
+      this.refreshing = true;
+
+      // Make sure we only send a 'skip waiting' message if the SW is waiting
+      // if (!this.registration || !this.registration.waiting) return;
+      // send message to SW to skip the waiting and activate the new SW
+      // this.registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+
+      // Here the actual reload of the page occurs
+      window.location.reload();
+    },
+  },
+};
