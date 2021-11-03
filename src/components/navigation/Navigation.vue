4f6273061e7adce78a96f5f66152905bd9d7c1a6
Sebastian Spreizer <spreizer@web.de>
2021-11-03 12:24:18 +0100

    Dark mode for Nav

--- src/components/navigation/Navigation.vue
+++ src/components/navigation/Navigation.vue
@@ -49,6 +49,7 @@
           <v-navigation-drawer
             absolute
             app
+            dark
             mini-variant
             mini-variant-width="64"
             mobile-breakpoint="64"
@@ -58,7 +59,6 @@
               <v-list nav>
                 <v-list-item
                   :class="{ 'v-list-item--active': state.navList === route.id }"
-                  :color="route.color"
                   :key="route.id"
                   :title="route.title"
                   @click="() => updatePageContent(route.id)"
