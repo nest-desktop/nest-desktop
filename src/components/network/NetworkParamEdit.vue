314375db32ce8de0fae62fdcb14e7f7333a3e963
Sebastian Spreizer <spreizer@web.de>
2021-12-10 15:16:26 +0100

    Remove white background for connection space in connection controller

--- src/components/network/NetworkParamEdit.vue
+++ src/components/network/NetworkParamEdit.vue
@@ -267,7 +267,6 @@
                   <v-btn
                     @click="() => connection.state.select()"
                     block
-                    color="white"
                     depressed
                     height="40"
                     tile
