89f18898d1ea08346b5aec55cfd1f42635409544
Sebastian Spreizer <spreizer@web.de>
2022-02-02 13:16:31 +0100

    Correct comment

--- src/main.ts
+++ src/main.ts
@@ -44,7 +44,7 @@ if (process.env.IS_ELECTRON) {
   };
   mountApp();
 } else {
-  // Load the data from public/config.json for the global config and mount the app.
+  // Load the data from config.json for the global config and mount the app.
   fetch(combineURLs(process.env.BASE_URL, 'config.json'))
     .then(response => response.json())
     .then(appConfig => (Vue.prototype.$appConfig = appConfig))
