65f18b510749ee92b131e565ebb483856203e5f4
Sebastian Spreizer <spreizer@web.de>
2022-03-17 09:57:20 +0100

    Use const

--- src/components/parameter/ParameterEdit.vue
+++ src/components/parameter/ParameterEdit.vue
@@ -551,7 +551,7 @@ export default Vue.extend({
       if (state.param) {
         return state.param.title;
       } else {
-        let text = state.options.label;
+        const text = state.options.label;
         return state.options.unit ? text + ` (${state.options.unit})` : text;
       }
     };
