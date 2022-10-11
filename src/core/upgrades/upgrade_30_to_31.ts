465371a0a0e0845d6cfbd9ff127bf66faab3f9e1
Sebastian Spreizer <spreizer@web.de>
2022-10-11 15:19:40 +0200

    Restructure upgrades

--- src/core/upgrades/upgrade_30_to_31.ts
+++ src/core/upgrades/upgrade_30_to_31.ts
@@ -1,8 +1,13 @@
-export function upgradeProject_30_to_31(project: any): any {
-  if (!/^[0-3]\.[0]+(.\d+)?$/.test(project.version)) {
+import { App } from '../app';
+
+const validateVersion = (version: string) =>
+  /^3\.0(\.\d+)?(\w+)?$/.test(version);
+
+export function upgradeProject_30_to_31(app: App, project: any): any {
+  if (!validateVersion(project.version)) {
     return project;
   }
 
-  project.version = '3.1.0';
+  project.version = '3.1';
   return project;
 }
