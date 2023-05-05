0e4b2cd3b468750f2259dc7f0e711793baf37ae3
Sebastian Spreizer <spreizer@web.de>
2023-05-05 21:37:45 +0200

    Add card

---
+++ src/components/common/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card
+    class="card"
+    color="primary"
+    variant="outlined"
+    rounded="0"
+    :style="{ borderLeftColor: state.color }"
+  >
+    <slot />
+  </v-card>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from "vue";
+import { reactive } from "vue";
+
+const props = defineProps(["color"]);
+
+const state = reactive({
+  color: "blue",
+});
+
+onMounted(() => {
+  state.color = props.color;
+});
+</script>
+
+<style>
+.card {
+  border-left-width: 4px !important;
+}
+</style>
