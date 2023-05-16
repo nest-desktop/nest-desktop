5523404c504e42f67567d858b9373e24fb1931e0
Sebastian Spreizer <spreizer@web.de>
2023-05-16 09:55:26 +0200

    Remove vue-scrollTo

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -11,13 +11,10 @@ import { loadFonts } from "./webfontloader";
 import vuetify from "./vuetify";
 import pinia from "../store";
 import router from "../router";
-import VueScrollTo from "vue-scrollto";
 
 // Types
 import type { App } from "vue";
 
 export function registerPlugins(app: App) {
   loadFonts();
-  app.use(vuetify).use(router).use(pinia).use(VueScrollTo, {
-    offset: -24,});
-}
+  app.use(vuetify).use(router).use(pinia)}
