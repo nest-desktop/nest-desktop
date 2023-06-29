2eb57dc1c0acd4854e095ed6fdcccb4773c8875e
Sebastian Spreizer <spreizer@web.de>
2023-06-29 16:31:29 +0200

    Add logger

---
+++ src/components/common/IconBtn.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-btn class="icon-btn d-flex flex-column">
+    <template #prepend>
+      <v-icon :icon="props.icon" class="ma-1" size="large" />
+    </template>
+    <slot />
+  </v-btn>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps(["icon"]);
+</script>
+
+<style lang="scss">
+.icon-btn {
+  .v-btn__prepend {
+    margin-inline-start: 0;
+    margin-inline-end: 0;
+  }
+}
+</style>
