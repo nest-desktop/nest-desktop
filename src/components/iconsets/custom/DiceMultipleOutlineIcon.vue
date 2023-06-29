a55962778cec06e3d564d9e52451d4914f2474ee
Sebastian Spreizer <spreizer@web.de>
2023-06-29 16:31:29 +0200

    Add logger

---
+++ src/components/iconsets/custom/DiceMultipleOutlineIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    aria-hidden="true"
+    focusable="false"
+    height="1em"
+    preserveAspectRatio="xMidYMid meet"
+    style="
+      -ms-transform: rotate(360deg);
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
+    "
+    viewBox="0 0 24 24"
+    width="1em"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M14 8c-.55 0-1-.45-1-1s.45-1 1-1s1 .45 1 1s-.45 1-1 1m-2 4v7H5v-7h7m.78-2H4.22C3.55 10 3 10.55 3 11.22v8.56c0 .67.55 1.22 1.22 1.22h8.56c.67 0 1.22-.55 1.22-1.22v-8.56c0-.67-.55-1.22-1.22-1.22m7-7h-8.56C10.55 3 10 3.55 10 4.22V8h2V5h7v7h-3v2h3.78c.67 0 1.22-.55 1.22-1.22V4.22C21 3.55 20.45 3 19.78 3M17 8c-.55 0-1-.45-1-1s.45-1 1-1s1 .45 1 1s-.45 1-1 1m0 3c-.55 0-1-.45-1-1s.45-1 1-1s1 .45 1 1s-.45 1-1 1M7 15c-.55 0-1-.45-1-1s.45-1 1-1s1 .45 1 1s-.45 1-1 1m3 3c-.55 0-1-.45-1-1s.45-1 1-1s1 .45 1 1s-.45 1-1 1"
+      fill="currentColor"
+    />
+    <rect fill="rgba(0, 0, 0, 0)" height="24" width="24" x="0" y="0" />
+  </svg>
+</template>
---
+++ src/nest/components/editor/NetworkParamEditor.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="networkParamEditor">
+    <v-toolbar color="transparent" density="compact">
+      <v-btn-toggle class="mx-1" mandatory v-model="nodes.state.elementTypeIdx">
+        <icon-btn
+          :icon="item.icon"
+          :key="index"
+          size="x-small"
+          v-for="(item, index) in nodeTypes"
+        >
+          {{ item.title }}
+        </icon-btn>
+      </v-btn-toggle>
+      <v-spacer />
+      <v-btn icon="mdi-dots-vertical" size="small" />
+    </v-toolbar>
+    <div :key="nodes.state.nodesLength">
+      <div :key="index" v-for="(node, index) in nodes.all">
+        <node-editor
+          :node="(node as Node)"
+          @mouseenter="node.state.focus()"
+          @mouseleave="node.nodes.unfocusNode()"
+          v-if="showNode(node as Node)"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+import { Node } from "@nest/core/node/node";
+import { useProjectStore } from "@nest/store/project/projectStore";
+
+import IconBtn from "@/components/common/IconBtn.vue";
+import NodeEditor from "@nest/components/editor/NodeEditor.vue";
+
+const projectStore = useProjectStore();
+
+const networkState = computed(() => projectStore.project.network.state);
+const nodes = computed(() => projectStore.project.network.nodes);
+
+const nodeTypes = [
+  { icon: "mdi-all-inclusive", id: "all", title: "all" },
+  { icon: "nest:stimulator", id: "stimulator", title: "stimulator" },
+  { icon: "mdi-shape", id: "neuron", title: "neuron" },
+  { icon: "nest:recorder", id: "recorder", title: "recorder" },
+  // { icon: "custom:copyModel", id: "model", title: "model" },
+];
+
+/**
+ * Show node in list.
+ */
+const showNode = (node: Node) => {
+  const elementTypeIdx = nodes.value.state.elementTypeIdx;
+
+  if (elementTypeIdx === 4) {
+    return false;
+  } else if (nodes.value.state.selectedNode) {
+    // selected view
+    return node.state.isSelected;
+  } else if (elementTypeIdx === 0) {
+    // all view
+    return true;
+  } else if (elementTypeIdx < nodeTypes.length) {
+    // element type view
+    return nodeTypes[elementTypeIdx].id === node.model.elementType;
+  } else {
+    // custom view
+    return networkState.value.state.displayIdx.nodes.includes(node.idx);
+  }
+};
+</script>
