d8c6e4980bc128d3a1828efbdb0792fb293fd309
Sebastian Spreizer <spreizer@web.de>
2023-09-14 17:18:43 +0200

    Add synapse components

--- src/helpers/synapse/synapse.ts
+++ src/helpers/synapse/synapse.ts
@@ -4,12 +4,6 @@ import { UnwrapRef, reactive } from "vue";
 import { ILogObj, Logger } from "tslog";
 
 import { Connection } from "@/types/connectionTypes";
-import { Model } from "@/types/modelTypes";
-import { ModelParameter } from "@/helpers/model/modelParameter";
-import {
-  SynapseParameter,
-  SynapseParameterProps,
-} from "@/helpers/synapse/synapseParameter";
 import { logger as mainLogger } from "@/helpers/common/logger";
 
 interface SynapseState {
@@ -17,63 +11,33 @@ interface SynapseState {
 }
 
 export interface SynapseProps {
-  model?: string;
-  params?: SynapseParameterProps[];
+  weight?: number;
 }
 
 export class BaseSynapse {
   private readonly _name = "Synapse";
   private _logger: Logger<ILogObj>;
-  private _modelId: string;
-  private _paramsVisible: string[] = [];
-  private _params: { [key: string]: SynapseParameter } = {};
   private _state: UnwrapRef<SynapseState>;
+  private _weight: number;
 
   public _connection: Connection; // parent
-  public _model: Model;
 
   constructor(connection: Connection, synapse?: SynapseProps) {
     this._connection = connection;
-    this._modelId = synapse?.model || "static_synapse";
 
     this._logger = mainLogger.getSubLogger({
-      name: `[${this._modelId}] synapse`,
+      name: `synapse`,
     });
 
     this._state = reactive({
       hash: "",
     });
 
-    this._model = this.getModel(this._modelId);
-    this.initParameters(synapse?.params);
+    this._weight = synapse?.weight || 1;
   }
 
   get connection(): Connection {
     return this._connection;
-  }
-
-  get delay(): number {
-    const delay: any = this._params.delay;
-    return delay ? delay.value : 1;
-  }
-
-  set delay(value: number) {
-    this._params.delay.value = value;
-  }
-
-  /**
-   * Returns all visible parameters.
-   */
-  get filteredParams(): SynapseParameter[] {
-    return this._paramsVisible.map((paramId) => this._params[paramId]);
-  }
-
-  get hasSomeVisibleParams(): boolean {
-    return this._paramsVisible.length > 0;
-  }
-
-  get hasSynSpec(): boolean {
-    return !this.isStatic || this.hasSomeVisibleParams;
   }
 
   get icon(): string {
@@ -93,82 +57,12 @@ export class BaseSynapse {
     return false;
   }
 
-  get isStatic(): boolean {
-    return this.model.id === "static_synapse";
-  }
-
   get logger(): Logger<ILogObj> {
     return this._logger;
   }
 
-  get model(): Model {
-    if (this._model?.id !== this._modelId) {
-      this._model = this.getModel(this._modelId);
-    }
-    return this._model;
-  }
-
-  /**
-   * Set model.
-   *
-   * @remarks
-   * It initializes parameters and activity components.
-   * It triggers node changes.
-   *
-   * @param model - synapse model
-   */
-  set model(model: Model) {
-    this._modelId = model.id;
-    this._model = model;
-    this.modelChanges();
-  }
-
-  get modelDBStore(): any {
-    return this.connection.connections.network.project.modelDBStore;
-  }
-
-  get modelId(): string {
-    return this._modelId;
-  }
-
-  /**
-   * Set model ID.
-   *
-   * @remarks
-   * It initializes parameters.
-   *
-   * @param value - ID of the model
-   */
-  set modelId(value: string) {
-    this._modelId = value;
-  }
-
-  get modelParams(): { [key: string]: ModelParameter } {
-    return this.model.params;
-  }
-
-  get models(): Model[] {
-    const elementType: string = this.model.elementType;
-    const models: Model[] =
-      this.modelDBStore.getModelsByElementType(elementType);
-    return models;
-  }
-
   get name(): string {
     return this._name;
-  }
-
-  get params(): { [key: string]: SynapseParameter } {
-    return this._params;
-  }
-
-  get paramsVisible(): string[] {
-    return this._paramsVisible;
-  }
-
-  set paramsVisible(values: string[]) {
-    this._paramsVisible = values;
-    this.changes();
   }
 
   get state(): UnwrapRef<SynapseState> {
@@ -176,16 +70,12 @@ export class BaseSynapse {
   }
 
   get weight(): number {
-    let weight: any = this._params.weight;
-    if (weight && !weight.visible) {
-      weight = this.model.params.weight;
-    }
-    return weight ? weight.value : 1;
+    return this._weight;
   }
 
-  // set weight(value: number) {
-  //   this._params.weight.value = value;
-  // }
+  set weight(value: number) {
+    this._weight = value;
+  }
 
   get weightColor(): string {
     if (this.connection.view.connectRecorder() || this.weight === 0) {
@@ -204,19 +94,8 @@ export class BaseSynapse {
   }
 
   set weightLabel(value: string) {
-    const weight: SynapseParameter = this.params.weight;
-    weight.visible = true;
-    weight.value =
-      (value === "inhibitory" ? -1 : 1) * Math.abs(weight.value as number);
-  }
-
-  /**
-   * Add model parameter component.
-   * @param param - parameter object
-   */
-  addParameter(param: SynapseParameterProps): void {
-    // this._logger.trace("add parameter:", param)
-    this._params[param.id] = new SynapseParameter(this, param);
+    this.weight =
+      (value === "inhibitory" ? -1 : 1) * Math.abs(this.weight as number);
   }
 
   /**
@@ -230,78 +109,20 @@ export class BaseSynapse {
     this.connection.changes();
   }
 
-  getModel(modelId: string): Model {
-    this._logger.trace("get model:", modelId);
-    return this.modelDBStore.getModel(modelId);
-  }
-
-  /**
-   * Sets all params to invisible.
-   */
-  hideAllParams(): void {
-    this._paramsVisible = [];
-  }
-
-  /**
-   * Initialize synapse parameters.
-   */
-  initParameters(params?: SynapseParameterProps[]): void {
-    this._logger.trace("init parameters");
-    this._paramsVisible = [];
-    this._params = {};
-    if (this.model && params) {
-      Object.values(this.model.params).forEach((modelParam: ModelParameter) => {
-        const param = params?.find((param: any) => param.id === modelParam.id);
-        this.addParameter(param || modelParam);
-        if (param && param.visible !== false) {
-          this._paramsVisible.push(modelParam.id);
-        }
-      });
-    } else if (this.model) {
-      Object.values(this.model.params).forEach((modelParam: ModelParameter) =>
-        this.addParameter(modelParam)
-      );
-    } else if (params) {
-      params.forEach((param: any) => this.addParameter(param));
-    }
-  }
-
   /**
    * Inverse synaptic weight.
    */
   inverseWeight(): void {
     this._logger.trace("inverse weight");
-    const weight: SynapseParameter = this._params.weight;
-    if (typeof weight.value === "number") {
-      weight.visible = true;
-      weight.value = -1 * weight.value;
-      this.connection.changes();
-    }
+    this.weight = -1 * this.weight;
+    this.connection.changes();
   }
 
   /**
-   * Observer for model changes.
-   *
-   * @remarks
-   * It emits synapse changes.
+   * Reset synaptic weight.
    */
-  modelChanges(): void {
-    this.initParameters();
-    this.connection.network.clean();
-    this.changes();
-  }
-
   reset(): void {
-    this.filteredParams.forEach((param: SynapseParameter) => param.reset());
-  }
-
-  /**
-   * Sets all params to visible.
-   */
-  showAllParams(): void {
-    Object.values(this.params).forEach(
-      (param: SynapseParameter) => (param.visible = true)
-    );
+    this.weight = 1;
   }
 
   /**
@@ -311,14 +132,8 @@ export class BaseSynapse {
   toJSON(): SynapseProps {
     const synapse: SynapseProps = {};
 
-    if (this._modelId !== "static_synapse") {
-      synapse.model = this._modelId;
-    }
-
-    if (this.filteredParams.length > 0) {
-      synapse.params = this.filteredParams.map((param: SynapseParameter) =>
-        param.toJSON()
-      );
+    if (this.weight !== 1) {
+      synapse.weight = this.weight;
     }
 
     return synapse;
--- src/types/synapseTypes.ts
+++ src/types/synapseTypes.ts
@@ -4,3 +4,6 @@ import { BaseSynapse } from "@/helpers/synapse/synapse";
 import { NESTSynapse } from "@/simulators/nest/helpers/synapse/synapse";
 
 export type Synapse = BaseSynapse | NESTSynapse;
+
+// for synapse spec editor
+export const SynapsePropTypes = [BaseSynapse, NESTSynapse];
