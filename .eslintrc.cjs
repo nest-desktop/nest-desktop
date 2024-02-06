c212294c6031e0087f21f1de4eb115fce55e9897
Sebastian Spreizer <spreizer@web.de>
2024-02-06 20:42:51 +0100

    Use CommonJS for these config files

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+  ],
+  rules: {
+    "vue/multi-word-component-names": "off",
+    "vue/valid-v-slot": [
+      "error",
+      {
+        allowModifiers: true,
+      },
+    ],
+  },
+};
---
+++ electron.vite.config.cts
@@ -0,0 +1,13 @@
+// electron.vite.config.js
+
+export default {
+  main: {
+    // vite config options
+  },
+  preload: {
+    // vite config options
+  },
+  renderer: {
+    // vite config options
+  }
+}
