88ebc69dc95311e3c50d9c8f6d7c23566160e965
Sebastian Spreizer <spreizer@web.de>
2024-07-16 11:23:17 +0200

    Update model assets

--- src/simulators/pynn/stores/model/modelDBStore.ts
+++ src/simulators/pynn/stores/model/modelDBStore.ts
@@ -6,20 +6,14 @@ import { PyNNModel } from "../../helpers/model/model";
 import { PyNNModelDB } from "../../helpers/model/modelDB";
 
 const modelAssets = [
-  "ac_generator",
-  "dc_generator",
-  "hh_psc_alpha",
-  "iaf_cond_alpha",
-  "iaf_psc_alpha",
-  "multimeter",
-  "noise_generator",
-  "parrot_neuron",
-  "poisson_generator",
-  "spike_generator",
-  "spike_recorder",
-  "static_synapse",
-  "step_current_generator",
-  "voltmeter",
+  "ACSource",
+  "DCSource",
+  "IF_cond_alpha",
+  "IF_curr_alpha",
+  "NoisyCurrentSource",
+  "SpikeSourceArray",
+  "SpikeSourcePoisson",
+  "StepCurrentSource",
 ];
 
 // export const usePyNNModelDBStore = defineModelDBStore<PyNNModel>({
