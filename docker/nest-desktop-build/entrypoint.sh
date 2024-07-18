cacd3c3d51b44ee55e432d3ac8d53773effa1d54
Sebastian Spreizer <spreizer@web.de>
2024-07-18 16:34:08 +0200

    Relocate docker

--- docker/nest-desktop-build/entrypoint.sh
+++ docker/nest-desktop-build/entrypoint.sh
@@ -4,7 +4,7 @@ jq -n \
   --arg simulator_visible "${SIMULATOR_VISIBLE}" \
   '{
     simulatorVisible: $simulator_visible
-  }' > /usr/share/nginx/html/assets/args/app.json
+  }' > /usr/share/nginx/html/assets/app.json
 
 bash /usr/local/bin/simulators/nest.sh
 bash /usr/local/bin/simulators/norse.sh
