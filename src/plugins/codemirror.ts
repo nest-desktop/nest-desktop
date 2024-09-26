86acdeac698d129bd3156af722c29460a8133073
Sebastian Spreizer <spreizer@web.de>
2024-09-26 16:57:09 +0200

    Add lang yaml to codemirror

--- src/plugins/codemirror.ts
+++ src/plugins/codemirror.ts
@@ -1,11 +1,13 @@
 // codemirror.ts
 
 import { basicSetup } from "codemirror";
+// import { nestml } from "codemirror-lang-nestml";
 import VueCodemirror from "vue-codemirror";
 
 import { autocompletion } from "@codemirror/autocomplete";
 import { json } from "@codemirror/lang-json";
 import { python } from "@codemirror/lang-python";
+import { yaml } from "@codemirror/lang-yaml";
 import { Compartment, Extension } from "@codemirror/state";
 import { oneDark } from "@codemirror/theme-one-dark";
 
@@ -15,21 +17,33 @@ import { zebraStripes } from "./codeMirrorExtensions/zebraStripes";
 
 export default VueCodemirror;
 
+function languageJSON(): Extension[] {
+  const language = new Compartment();
+  return [language.of(json())];
+}
+
+// function languageNESTML(): Extension[] {
+//   const language = new Compartment();
+//   return [language.of(nestml())];
+// }
+
 function languagePython(): Extension[] {
   const language = new Compartment();
   return [language.of(python())];
 }
 
-function languageJSON(): Extension[] {
+function languageYAML(): Extension[] {
   const language = new Compartment();
-  return [language.of(json())];
+  return [language.of(yaml())];
 }
 
 export {
   autocompletion,
   basicSetup,
   languageJSON,
+  // languageNESTML,
   languagePython,
+  languageYAML,
   oneDark,
   zebraStripes,
   highlightLine,
