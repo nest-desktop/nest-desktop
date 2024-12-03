4800fac9bb01ce10afa76284b83da29297bbbdde
Sebastian Spreizer <spreizer@web.de>
2024-12-03 13:13:06 +0100

    Add packages for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,43 @@
-// eslint.config.mjs
-import pluginVue from "eslint-plugin-vue";
+import eslint from '@eslint/js';
+// import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
+import typescriptEslint from 'typescript-eslint';
 
-export default [
-  ...pluginVue.configs["flat/recommended"],
+export default typescriptEslint.config(
   {
-    rules: {
-      "vue/multi-word-component-names": "off",
-      // "vue/valid-v-slot": [
-      //   "error",
-      //   {
-      //     allowModifiers: true,
-      //   },
-      // ],
-    },
     ignores: [
       ".gitignore",
-      ".github/*",
-      "coverage/*",
-      "dev-dist/*",
-      "dist-electron/*",
-      "nest_desktop.egg-info/*",
-      "node_modules/*",
-      "release/*",
+      ".github",
+      "**/coverage",
+      "**/dev-dist",
+      "**/dist",
+      "**/dist-electron",
+      "**/nest_desktop",
+      "**/nest_desktop.egg-info",
+      "**/node_modules",
+      "**/release",
+      "*.d.ts"
     ],
+  }, {
+  extends: [
+    eslint.configs.recommended,
+    ...typescriptEslint.configs.recommended,
+    ...eslintPluginVue.configs['flat/recommended'],
+  ],
+  files: ['**/*.{ts,vue}'],
+  languageOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    globals: globals.browser,
+    parserOptions: {
+      parser: typescriptEslint.parser,
+    },
   },
-];
+  rules: {
+    // your rules
+    "vue/multi-word-component-names": "off",
+  },
+  // eslintConfigPrettier
+}
+)
