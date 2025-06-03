a6ad3a71453337e14df02618249778377ab2ef02
Sebastian Spreizer <spreizer@web.de>
2025-06-03 10:47:58 +0200

    Remove neo from elephant workspace

--- src/workspaces/elephant/helpers/codeNodeTypes/neo/interfaceTypes.ts
+++
@@ -1,9 +0,0 @@
-// Create the types. It is recommended to define them
-// in a separate file and import them when creating the nodes.
-import { NodeInterfaceType } from "@baklavajs/interface-types";
-
-export interface IAnalogSignal {}
-export const analogSignal = new NodeInterfaceType<IAnalogSignal>("analogSignal");
-
-export interface ISpikeTrain {}
-export const spikeTrain = new NodeInterfaceType<ISpikeTrain>("spikeTrain");
