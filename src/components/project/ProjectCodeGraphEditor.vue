00d0ac7a2f5a83b8894e4e16f0962a54a2af725f
Sebastian Spreizer <spreizer@web.de>
2025-11-06 11:38:25 +0100

    Add code graph editor for project

---
+++ src/components/project/ProjectCodeGraphEditor.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-layout id="ProjectCodeGraphEditor" full-height>
+    <CodeGraphEditor :view-model>
+      <template #sidebarCodeEditor="{ node }">
+        <CodeEditor v-model="node.script" :locked="node.lockCode" @update:locked="(v) => (node.lockCode = v)" />
+      </template>
+    </CodeGraphEditor>
+  </v-layout>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { CodeGraphEditor } from "@babsey/code-graph";
+
+import CodeEditor from "@/codeGraph/components/CodeEditor.vue";
+
+import { useCodeGraphStore } from "@/stores/graph/codeGraphStore";
+const codeGraphStore = useCodeGraphStore();
+const viewModel = computed(() => codeGraphStore.viewModel);
+</script>
