4e5356cfb784456d06d4e51ac2e2e40384a5807e
Sebastian Spreizer <spreizer@web.de>
2025-11-06 11:40:21 +0100

    Add project graph editor view

---
+++ src/workspaces/nest/views/ProjectGraphEditor.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-layout id="ProjectGraphEditor" full-height>
+    <NetworkGraphEditor v-if="projectViewStore.state.views.graph === 'network'" />
+    <ProjectCodeGraphEditor v-else-if="projectViewStore.state.views.graph === 'code'" />
+  </v-layout>
+</template>
+
+<script setup lang="ts">
+import ProjectCodeGraphEditor from "@/components/project/ProjectCodeGraphEditor.vue";
+
+import NetworkGraphEditor from "../components/network/NetworkGraphEditor.vue";
+
+import { useAppStore } from "@/stores/appStore";
+const appStore = useAppStore();
+const projectViewStore = appStore.currentWorkspace?.views.project;
+</script>
