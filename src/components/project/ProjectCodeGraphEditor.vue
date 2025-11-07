2711b1b7b2716c12d6af4b6fe9ad54eb63af6741
Sebastian Spreizer <spreizer@web.de>
2025-11-07 14:29:32 +0100

    Update project code graph editor

--- src/components/project/ProjectCodeGraphEditor.vue
+++ src/components/project/ProjectCodeGraphEditor.vue
@@ -1,6 +1,6 @@
 <template>
   <v-layout id="ProjectCodeGraphEditor" full-height>
-    <CodeGraphEditor :view-model>
+    <CodeGraphEditor :view-model="codeViewModel">
       <template #sidebarCodeEditor="{ node }">
         <CodeEditor v-model="node.script" :locked="node.lockCode" @update:locked="(v) => (node.lockCode = v)" />
       </template>
@@ -14,7 +14,9 @@ import { CodeGraphEditor } from "@babsey/code-graph";
 
 import CodeEditor from "@/codeGraph/components/CodeEditor.vue";
 
-import { useCodeGraphStore } from "@/stores/graph/codeGraphStore";
-const codeGraphStore = useCodeGraphStore();
-const viewModel = computed(() => codeGraphStore.viewModel);
+import { useAppStore } from "@/stores/appStore";
+const appStore = useAppStore();
+const projectStore = computed(() => appStore.currentWorkspace.stores.projectStore);
+const project = computed(() => projectStore.value.state.project);
+const codeViewModel = computed(() => project.value.code.viewModel);
 </script>
