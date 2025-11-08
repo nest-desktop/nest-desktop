cecdbe3967c02a7c42d28cf6cfd3acb88cc0cb2f
Sebastian Spreizer <spreizer@web.de>
2025-11-08 14:36:22 +0100

    Update plugins

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -20,8 +20,8 @@ import { loadFonts } from "./webfontloader";
 
 import "splitpanes/dist/splitpanes.css";
 // import "@baklavajs/themes/dist/classic.css";
-import '@baklavajs/themes/dist/syrup-dark.css';
-import '@babsey/code-graph/style.css';
+import "@baklavajs/themes/dist/syrup-dark.css";
+import "@babsey/code-graph/style.css";
 
 export function registerPlugins(app: App) {
   // Load fonts
