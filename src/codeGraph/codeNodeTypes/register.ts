7b01b0eedeb2093e7cda77bba810c033d7dfcfd6
Sebastian Spreizer <spreizer@web.de>
2026-01-07 14:51:49 +0100

    Update registering code types

--- src/codeGraph/codeNodeTypes/register.ts
+++ src/codeGraph/codeNodeTypes/register.ts
@@ -4,21 +4,23 @@ import { type Ref, ref } from "vue";
 import { type ICodeGraphViewModel } from "@babsey/code-graph";
 
 import { registerDefaultNodeTypes } from "./default";
-import { registerExampleNodeTypes } from "./examples";
-import { registerNumpyNodeTypes } from "./numpy";
 import { registerElephantNodeTypes } from "./elephant";
-import { registerTorchNodeTypes } from "./torch";
+import { registerExampleNodeTypes } from "./examples";
+import { registerHumamNodeTypes } from "./humam";
 import { registerNeoNodeTypes } from "./neo";
+import { registerNumpyNodeTypes } from "./numpy";
+import { registerTorchNodeTypes } from "./torch";
 
 type TCodeNodeModules = (viewModel: ICodeGraphViewModel) => void;
 
 const codeNodeModules: Ref<Record<string, TCodeNodeModules>> = ref({
   default: registerDefaultNodeTypes,
-  example: registerExampleNodeTypes,
-  numpy: registerNumpyNodeTypes,
   elephant: registerElephantNodeTypes,
-  torch: registerTorchNodeTypes,
+  example: registerExampleNodeTypes,
+  humam: registerHumamNodeTypes,
   neo: registerNeoNodeTypes,
+  numpy: registerNumpyNodeTypes,
+  torch: registerTorchNodeTypes,
 });
 
 export const registerCodeNodeModule = (moduleName: string, nodeTypes: TNodeTypeModule) => {
