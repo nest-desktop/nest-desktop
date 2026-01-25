cf32a53c561bcb76d3c66e4a8f7227c27a681453
Sebastian Spreizer <spreizer@web.de>
2026-01-25 12:48:39 +0100

    Update pynn workspace

--- src/workspaces/pynn/layouts/ProjectLayout.vue
+++ src/workspaces/pynn/layouts/ProjectLayout.vue
@@ -90,7 +90,7 @@ onMounted(() => {
 
   if (project.value.viewModel.subscribe) project.value.viewModel.subscribe();
   project.value.viewModel.engine?.start();
-  project.value.viewModel.engine?.runOnce(null);
+  // project.value.viewModel.engine?.runOnce({});
 });
 
 onBeforeUnmount(() => {
@@ -108,7 +108,7 @@ watch(
 
     newValue.viewModel.subscribe();
     newValue.viewModel.engine?.start();
-    setTimeout(() => newValue.viewModel.engine?.runOnce(null), 1);
+    // setTimeout(() => newValue.viewModel.engine?.runOnce({}), 1);
   },
 );
 
