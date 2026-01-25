4999536320de8d509a85f32507df69b98625ecf4
Sebastian Spreizer <spreizer@web.de>
2026-01-25 12:46:46 +0100

    Update array input

--- src/components/controls/ArrayInput.vue
+++ src/components/controls/ArrayInput.vue
@@ -1,47 +1,17 @@
 <template>
   <v-textarea
-    v-model="value"
+    v-model="model"
     :rows="1"
     base-color="grey"
     class="mx-1 array-input"
     color="grey"
     density="compact"
     hide-details
-    @blur="emitUpdate()"
-    @keydown.enter="emitUpdate"
   />
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-
-const emit = defineEmits(["update:modelValue"]);
-const props = defineProps({
-  modelValue: { default: [], type: Array<number> },
-  unit: { default: "", type: String },
-});
-
-const value = ref(props.modelValue);
-
-const emitUpdate = (k?: KeyboardEvent) => {
-  if (props.modelValue === value.value) return;
-  k?.preventDefault();
-
-  let valueEmit: number[] = [];
-
-  switch (typeof value.value) {
-    case "number":
-      valueEmit = [value.value];
-      break;
-    case "string":
-      valueEmit =
-        // @ts-expect-error Property 'startsWith' does not exist on type 'never'.
-        value.value.startsWith("[") && value.value.endsWith("]") ? JSON.parse(value) : JSON.parse(`[${value.value}]`);
-      break;
-  }
-
-  emit("update:modelValue", valueEmit);
-};
+const model = defineModel<string>({ required: true });
 </script>
 
 <style lang="scss">
